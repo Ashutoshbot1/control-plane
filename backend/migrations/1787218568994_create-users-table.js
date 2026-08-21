897de0283be1302314227009d488eacc5c388e70
Ashutoshbot1 <ashutosh21jh@gmail.com>
2026-08-21 19:04:28 +0530

    added complete migaration for user table

--- backend/migrations/1787218568994_create-users-table.js
+++ backend/migrations/1787218568994_create-users-table.js
@@ -10,6 +10,38 @@ export const shorthands = undefined;
  */
 export const up = (pgm) => {
   pgm.createType("user_role", ["SUPER_ADMIN", "ADMIN", "USER"]);
+  pgm.createTable("users", {
+    id: "id",
+    name: {
+      type: "varchar(255)",
+      notNull: true,
+    },
+    email: {
+      type: "varchar(255)",
+      notNull: true,
+      unique: true,
+    },
+    password_hash: {
+      type: "text",
+    },
+    role: {
+      type: "user_role",
+      notNull: true,
+    },
+    deactivated_at: {
+      type: "timestamptz",
+    },
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
 };
 
 /**
@@ -18,5 +50,6 @@ export const up = (pgm) => {
  * @returns {Promise<void> | void}
  */
 export const down = (pgm) => {
+  pgm.dropTable("users");
   pgm.dropType("user_role");
 };
